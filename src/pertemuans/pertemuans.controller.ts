0999af6d1c1ec501d01591101fb93ababbd47e7f
IndraAjiyanto <indraajiyanto052@gmail.com>
2025-08-07 09:20:56 +0700

    update:view user page and kelas page

--- src/pertemuans/pertemuans.controller.ts
+++ src/pertemuans/pertemuans.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Res, Req } from '@nestjs/common';
 import { PertemuansService } from './pertemuans.service';
 import { CreatePertemuanDto } from './dto/create-pertemuan.dto';
 import { UpdatePertemuanDto } from './dto/update-pertemuan.dto';
 import { AuthenticatedGuard } from 'src/common/guards/authentication.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { Response } from 'express';
 
 @UseGuards(AuthenticatedGuard)
 @Controller('pertemuans')
@@ -12,14 +13,16 @@ export class PertemuansController {
 
   @Roles('admin')
   @Post()
-  create(@Body() createPertemuanDto: CreatePertemuanDto) {
-    return this.pertemuansService.create(createPertemuanDto);
+  async create(@Body() createPertemuanDto: CreatePertemuanDto, @Res() res:Response) {
+    await this.pertemuansService.create(createPertemuanDto);
+    res.redirect('pertemuans')
   }
 
   @Roles('admin')
   @Get()
-  findAll() {
-    return this.pertemuansService.findAll();
+  async findAll(@Res() res:Response, @Req() req:any) {
+    const pertemuan = await this.pertemuansService.findAll();
+    res.render('pertemuan/index', {user: req.user, pertemuan})
   }
 
   @Roles('user', 'admin')
@@ -30,13 +33,16 @@ export class PertemuansController {
 
   @Roles('admin')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePertemuanDto: UpdatePertemuanDto) {
-    return this.pertemuansService.update(+id, updatePertemuanDto);
+  async update(@Param('id') id: number, @Body() updatePertemuanDto: UpdatePertemuanDto, @Res() res:Response) {
+    await this.pertemuansService.update(id, updatePertemuanDto);
+    res.redirect('pertemuans')
   }
 
   @Roles('admin')
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pertemuansService.remove(+id);
+  async remove(@Param('id') id: number, @Res() res:Response) {
+    await this.pertemuansService.remove(id);
+    res.redirect('pertemuans')
+
   }
 }
