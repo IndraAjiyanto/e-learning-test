1b5bbb750a157fb68e7bc2e140995e3a723c2f99
IndraAjiyanto <indraajiyanto052@gmail.com>
2026-02-24 13:08:08 +0700

    feat: add max-width utility for images and enhance header styles in edjs parser

--- src/common/public/js/edjs.ts
+++ src/common/public/js/edjs.ts
@@ -13,13 +13,14 @@ const parser = edjsHTML({
     return `
     <div class="my-4 flex justify-center ${classList.join(" ")}">
               <div class="flex justify-center mb-12">
-            <div class="relative rounded-2xl overflow-hidden w-full sm:w-4/5 md:w-3/4 lg:w-2/3 flex justify-center items-center">
+            <div class="relative rounded-2xl overflow-hidden max-w-[300px] flex justify-center items-center">
       <img 
         src="${file.url}" 
         alt="${caption || "Image"}"
         class="h-auto object-contain"
         loading="lazy"
       />
+      ${caption ? `<p class="mt-2 text-sm text-center text-gray-600">${caption}</p>` : ""}
       </div>
       </div>
       </div>
@@ -27,17 +28,17 @@ const parser = edjsHTML({
   },
   paragraph: (block) => {
     const align = block.data.alignment;
-    return `<p class="mb-3 text-lg text-gray-800 text-${align}">${block.data.text}</p>`;
+    return `<p class="mb-3 text-lg text-gray-800 [&_a]:underline text-${align}">${block.data.text}</p>`;
   },
   header: (block) => {
     const level = block.data.level;
-    if(level === 1) return `<h1 class="mb-4 text-3xl leading-9 font-bold">${block.data.text}</h1>`;
-    if(level === 2) return `<h2 class="mb-4 text-2xl leading-8 font-bold">${block.data.text}</h2>`;
-    if(level === 3) return `<h3 class="mb-4 text-xl leading-7 font-bold">${block.data.text}</h3>`;
-    if(level === 4) return `<h4 class="mb-2 text-lg leading-7 font-semibold">${block.data.text}</h4>`;
-    if(level === 5) return `<h5 class="mb-2 text-base leading-6 font-medium">${block.data.text}</h5>`;
-    if(level === 6) return `<h6 class="mb-2 text-sm leading-5 font-medium">${block.data.text}</h6>`;
-    return `<h${level} class="mb-4 text-4xl font-bold text-gray-900">${block.data.text}</h${level}>`;
+    if(level === 1) return `<h1 class="mb-4 text-gray-900 text-3xl leading-9 font-bold [&_a]:underline">${block.data.text}</h1>`;
+    if(level === 2) return `<h2 class="mb-4 text-gray-900 text-2xl leading-8 font-bold [&_a]:underline">${block.data.text}</h2>`;
+    if(level === 3) return `<h3 class="mb-4 text-gray-900 text-xl leading-7 font-bold [&_a]:underline">${block.data.text}</h3>`;
+    if(level === 4) return `<h4 class="mb-2 text-gray-900 text-lg leading-7 font-semibold [&_a]:underline">${block.data.text}</h4>`;
+    if(level === 5) return `<h5 class="mb-2 text-gray-900 text-base leading-6 font-medium [&_a]:underline">${block.data.text}</h5>`;
+    if(level === 6) return `<h6 class="mb-2 text-gray-900 text-sm leading-5 font-medium [&_a]:underline">${block.data.text}</h6>`;
+    return `<h1 class="mb-4 text-gray-900 text-4xl font-bold">${block.data.text}</h1>`;
   }
 });
 
