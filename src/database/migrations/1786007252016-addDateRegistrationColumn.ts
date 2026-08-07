406fca9ea8866a3fe12a6de67c964a6016c0f886
johncalvins <computer.id349@gmail.com>
2026-08-07 10:16:33 +0700

    Refactor code structure for adding column current _status and attend_program to payments table

--- src/database/migrations/1786007252016-addDateRegistrationColumn.ts
+++ src/database/migrations/1786007252016-addDateRegistrationColumn.ts
@@ -3,6 +3,23 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddDateRegistrationColumn1786007252016 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+       await queryRunner.query(`
+            CREATE TYPE "payments_current_status_enum" AS ENUM(
+                'University Student', 'Fresh Graduate', 'Job Seeker', 
+                'Employee', 'Freelancer', 'Entrepreneur', 'Other'
+            )
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "payments" 
+            ADD "current_status" "payments_current_status_enum" NULL
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE "payments" 
+            ADD "attend_program" boolean NULL
+        `);
+
          await queryRunner.query(`
       ALTER TABLE "course"
       ADD COLUMN "date_registration" TIMESTAMP NULL;
@@ -14,6 +31,16 @@ export class AddDateRegistrationColumn1786007252016 implements MigrationInterfac
       ALTER TABLE "course"
       DROP COLUMN "date_registration";
     `);        
+
+      await queryRunner.query(`
+            ALTER TABLE "payments" DROP COLUMN "attend_program"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "payments" DROP COLUMN "current_status"
+        `);
+        await queryRunner.query(`
+            DROP TYPE "payments_current_status_enum"
+        `);
     }
 
 }
