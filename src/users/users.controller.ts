a64dc53bc5aeacd255923efd3417334234201ea5
johncalvins <computer.id349@gmail.com>
2026-08-10 16:20:39 +0700

    Implement email verification success page and update redirect path

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -171,14 +171,23 @@ export class UsersController {
   ) {
     try {
       await this.usersService.verifyEmail(token);
+      console.log('teeeeee');
       req.flash('success', 'Email verified successfully! You can now login.');
-      res.redirect('/login');
+      res.redirect('/users/verify-email-success');
     } catch (error: any) {
       const user = await this.usersService.findUserByEmail(email);
       req.flash('error', error.message || 'Email verification failed');
       res.redirect('/users/send-verify-email?token=' + user.verificationToken);
     }
   }
+
+  @Get('verify-email-success')
+  async verifyEmailSuccess(
+    @Res() res: Response
+  ){
+    res.render('verify-email-success')
+  }
+
 
   // ============================================
   // PROTECTED ROUTES - Require Authentication
