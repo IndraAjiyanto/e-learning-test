9ce0a6da076803786e44b1e019d11914d35f117b
johncalvins <computer.id349@gmail.com>
2026-08-10 13:54:13 +0700

    Refactor code structure for improved readability and maintainability and create verify email successfully page.

--- src/app.module.ts
+++ src/app.module.ts
@@ -236,6 +236,7 @@ export class AppModule implements OnApplicationBootstrap {
         '/register/:id',
         '/session-expired',
         '/users/send-verify-email',
+        '/users/verify-email-success',
         '/users/verify-email',
         '/users/reset-password',
         '/users/forgot-password',
