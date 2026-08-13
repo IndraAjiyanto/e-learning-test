f56a7cec06b20fb41c2451abb316ce10d367457d
johncalvins <computer.id349@gmail.com>
2026-08-13 13:14:57 +0700

    feat(gallery): add 'no' field to gallery entity and update related DTOs and views

--- src/entities/gallery.entity.ts
+++ src/entities/gallery.entity.ts
@@ -1,6 +1,7 @@
 import { Exclude } from "class-transformer";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Category } from "./category.entity";
+import { noGallery } from "./types/no-gallery";
 
 @Entity('gallery')
 export class Gallery {
@@ -15,6 +16,9 @@ export class Gallery {
 
     @Column({nullable:true})
     description: string
+
+    @Column({type:'enum', enum: [ '1', '2', '3', '4', '5', '6' ]})
+    no:noGallery
 
      @ManyToOne(() => Category, (category) => category.gallery, {
     nullable: true,
