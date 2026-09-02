16b617f5597224bb973deee03ce019450164f239
IndraAjiyanto <indraajiyanto052@gmail.com>
2026-09-02 16:13:29 +0700

    refactor(invoice): use PaymentsService.addUserToCourse for user enrollment

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Invoice } from 'src/entities/invoice.entity';
 import { Payment } from 'src/entities/payment.entity';
@@ -8,12 +8,14 @@ import { InvoiceController } from './invoice.controller';
 import { ConfigModule } from '@nestjs/config';
 import { VoucherModule } from 'src/voucher/voucher.module';
 import { Course } from 'src/entities/course.entity';
+import { PaymentsModule } from 'src/payments/payments.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Invoice, Payment, UserCourse, Course]),
     ConfigModule,
     VoucherModule,
+    forwardRef(() => PaymentsModule),
   ],
   controllers: [InvoiceController],
   providers: [InvoiceService],
